05271adcda71711eaa4f74cfeb0283e11c94b724
Javier Cortés Tejada <josejaco@ucm.es>
2016-12-12 19:15:03 +0100

    Script en bash para parte obligatoria
    Script terminado, queda por ver si todo va bien

---
+++ script.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+BASE_DIR="./data" # directorio con los ficheros base
+TEMP_DIR="./temp" # directorio para archivos temporales
+RESL_DIR="./result" # directorio resultado
+
+MY_FS_H="myFS.h"
+FUSE_LIB_C="fuseLib.c"
+AUX_FILE_C="hi.c"
+
+# restauramos el directorio resultado
+rmdir $RESL_DIR 
+mkdir $RESL_DIR 
+
+
+# a: 
+ 
+# copiamos uno de los archivos del dir base al dir. temporal y resultado
+cp $BASE_DIR/$MY_FS_H $TEMP_DIR/$MY_FS_H	
+cp $BASE_DIR/$MY_FS_H $RESL_DIR/$MY_FS_H
+
+# copiamos el otro archivo del dir base al dir. temporal y resultado
+cp $BASE_DIR/$FUSE_LIB_C $TEMP_DIR/$FUSE_LIB_C	
+cp $BASE_DIR/$FUSE_LIB_C $RESL_DIR/$FUSE_LIB_C
+
+
+# b:
+
+# montamos un sistema de ficheros
+./my-fsck virtual-disk
+
+# hacemos diff de ambos ficheros sobre los directorios donde fueron copiados (si tenemos como resultado cadena vacia todo bien, mirar en internet si dudas)
+DIFF_MY_FS_H=$(diff $RESL_DIR/$MY_FS_H $TEMP_DIR/$MY_FS_H)
+DIFF_FUSE_LIB_C=$(diff $RESL_DIR/$FUSE_LIB_C $TEMP_DIR/$FUSE_LIB_C)
+
+if [ "$DIFF_MY_FS_H" == "" ]; 
+	then echo " <MY_FS_H> Los ficheros son iguales en ambos directorios"
+else echo " <MY_FS_H> Los ficheros son distintos"
+fi
+
+if [ "$DIFF_FUSE_LIB_C" == "" ]; 
+	then echo " <FUSE_LIB_C> Los ficheros son iguales en ambos directorios"
+else echo " <FUSE_LIB_C> Los ficheros son distintos"
+fi
+
+# truncamos el mismo fichero en los dos directorios
+truncate --size=-4096 $TEMP_DIR/$MY_FS_H
+truncate --size=-4096 $RESL_DIR/$MY_FS_H
+
+echo " <MY_FS_H> Fichero truncado, ocupa un bloque menos (4096)"
+
+# c:
+
+# montamos un sistema de ficheros
+./my-fsck virtual-disk
+
+DIFF_TRUNCATE=$(diff $BASE_DIR/$MY_FS_H $RESL_DIR/$MY_FS_H)
+
+if [ "$DIFF_TRUNCATE" == "" ]; 
+	then echo " <MY_FS_H> El archivo es identico al original"
+else echo " <MY_FS_H> El archivo y la copia son distintos"
+fi
+
+# d:
+
+# copiamos un archivo cualquiera del dir base al dir resultado
+cp $BASE_DIR/$AUX_FILE_C $RESL_DIR/$AUX_FILE_C
+
+# e:
+
+# montamos un sistema de ficheros
+./my-fsck virtual-disk
+
+DIFF_COPY=$(diff $BASE_DIR/$AUX_FILE_C $RESL_DIR/$AUX_FILE_C)
+
+if [ "$DIFF_COPY" == "" ]; 
+	then echo " <AUX_FILE_C> La copia es identica al original."
+else echo " <AUX_FILE_C> La copia es distinta."
+fi
+
+# f:
+
+# truncamos el mismo fichero en los dos directorios
+truncate --size=+4096 $TEMP_DIR/$FUSE_LIB_C
+truncate --size=+4096 $RESL_DIR/$FUSE_LIB_C
+
+echo " <FUSE_LIB_C> Fichero truncado, ocupa un bloque mas(4096)"
+
+# g:
+
+# montamos un sistema de ficheros
+./my-fsck virtual-disk
+
+DIFF_TEMP_DIR_TRUNCATE_FUSE_LIB_C=$(diff $BASE_DIR/$FUSE_LIB_C $TEMP_DIR/$FUSE_LIB_C) # See Link1 at bottom | Make diff
+DIFF_RESL_DIR_TRUNCATE_FUSE_LIB_C=$(diff $BASE_DIR/$FUSE_LIB_C $RESL_DIR/$FUSE_LIB_C) # See Link1 at bottom | Make diff
+
+if [ "$DIFF_TEMP_DIR_TRUNCATE_FUSE_LIB_C" == "" ]; 
+	then echo "[FUSE_LIB_C -- TEMP_DIR] El fichero truncado es igual al original"
+else echo "[FUSE_LIB_C -- TEMP_DIR] Los ficheros son distintos"
+fi
+
+if [ "$DIFF_RESL_DIR_TRUNCATE_FUSE_LIB_C" == "" ]; 
+	then echo "[FUSE_LIB_C -- RESL_DIR] El fichero truncado es igual al original"
+else echo "[FUSE_LIB_C -- RESL_DIR] Los ficheros son distintos"
+fi
+
+
+
+
+
