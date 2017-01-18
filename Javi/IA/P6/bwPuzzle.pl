ba54c5ea2b0782bbcaad66d0ccef61aa9d83b17f
pedagova <permuta.yt@gmail.com>
2017-01-18 15:51:33 +0100

    Update bwPuzzle.pl

--- Javi/IA/P6/bwPuzzle.pl
+++ Javi/IA/P6/bwPuzzle.pl
@@ -1,30 +1,30 @@
-movimiento(estado(L, R), estado(FL, FR), 'iii'):-
+movimiento(estado(L, R), estado(FL, FR), 2,'iii'):-
 	reverse(L, [A,B,C|S]),
 	append([B, A, C], R, FR),
 	reverse(S, TEMP),
 	append(TEMP,[] , FL).
 
-movimiento(estado(L, R), estado(FL, FR), 'ii'):-
+movimiento(estado(L, R), estado(FL, FR), 1,'ii'):-
 	reverse(L, [A,B|S]),
 	append([A, B], R, FR),
 	reverse(S, TEMP),
 	append(TEMP, [] ,FL).
 
-movimiento(estado(L, R), estado(FL, FR), 'i'):-
+movimiento(estado(L, R), estado(FL, FR), 1,'i'):-
 	reverse(L, [A|S]),
 	append([A], R, FR),
 	reverse(S, TEMP),
 	append(TEMP, [] ,FL).
 
-movimiento(estado(L, [A, B, C| S]), estado(FL, FR), 'ddd'):-
+movimiento(estado(L, [A, B, C| S]), estado(FL, FR),2, 'ddd'):-
 	append(L, [C, A, B], FL),
 	append(S, [] ,FR).
 
-movimiento(estado(L, [A, B| S]), estado(FL, FR), 'dd'):-
+movimiento(estado(L, [A, B| S]), estado(FL, FR), 1,'dd'):-
 	append(L, [B, A], FL),
 	append(S, [] ,FR).
 
-movimiento(estado(L, [A| S]), estado(FL, FR), 'd'):-
+movimiento(estado(L, [A| S]), estado(FL, FR), 1,'d'):-
 	append(L, [A], FL),
 	append(S, [] ,FR).
 
