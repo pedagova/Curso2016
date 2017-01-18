66c8104d392c2f6c374111dadde883fd2a155f1f
pedagova <permuta.yt@gmail.com>
2017-01-18 15:08:41 +0100

    Update bwPuzzle.pl

--- Javi/IA/P6/bwPuzzle.pl
+++ Javi/IA/P6/bwPuzzle.pl
@@ -1,28 +1,62 @@
 try3([A, B, C | S], [A, B, C], S).
 try1([A|S], [A], S).
 try2([A, B|S],[A, B], S).
-movimiento(estado([XL | XLS], [XR|XRS]), estado([YL | YLS],[YR|YRS])):-
-	length([XL|XLS], K),
-	((K > 2) ->
-	reverse([XL|XLS], R),
-	try3(R, P1, P2),
-	reverse(P1, [P1OK| P1OKS]),
-	append([P1OK], [XR|XRS], AUX),
-	append(P1OKS, AUX, [YR|YRS]),
-	reverse(P2, AUX2),
-	append(AUX2, [], [YL|YLS])).
 
-
-/*
- * OPERADORES: movimiento(Estado, EstadoSiguiente)
- */
-movimiento(estado([H_LEFT | T_LEFT], [H_RIGTH | T_RIGTH]), estado([H_LEFT_RT | T_LEFT_RT],[H_RIGTH_RT|T_RIGTH_RT])):-
+movimiento(estado([H_LEFT | T_LEFT], T_RIGTH), estado(H_LEFT_RT,T_RIGTH_RT), 'iii'):-
 	length([H_LEFT |T_LEFT], LENGTH),
 	((LENGTH > 2) ->
 	reverse([H_LEFT |T_LEFT], REVERSE_RT),
-	try(REVERSE_RT, CHAIN1, TRY_RT),
+	try3(REVERSE_RT, CHAIN1, TRY_RT),
 	reverse(CHAIN1, [H_AUX| T_AUX]),
-	append([H_AUX], [H_RIGTH | T_RIGTH], APPEND_RT),
-	append(T_AUX, APPEND_RT, [H_RIGTH_RT | T_RIGTH_RT]),
+	append([H_AUX], T_RIGTH, APPEND_RT),
+	append(T_AUX, APPEND_RT, T_RIGTH_RT),
 	reverse(TRY_RT, AUX),
-	append(AUX, [], [H_LEFT_RT|T_LEFT_RT])).
+	append(AUX, [], H_LEFT_RT)).
+
+
+movimiento(estado([H_LEFT | T_LEFT], T_RIGTH), estado(H_LEFT_RT,T_RIGTH_RT), 'ii'):-
+	length([H_LEFT |T_LEFT], LENGTH),
+	((LENGTH > 1) ->
+	reverse([H_LEFT |T_LEFT], REVERSE_RT),
+	try2(REVERSE_RT, CHAIN1, TRY_RT),
+	reverse(CHAIN1, [H_AUX| T_AUX]),
+	append([H_AUX], T_RIGTH, APPEND_RT),
+	append(T_AUX, APPEND_RT, T_RIGTH_RT),
+	reverse(TRY_RT, AUX),
+	append(AUX, [], H_LEFT_RT)).
+
+
+movimiento(estado([H_LEFT | T_LEFT], T_RIGTH), estado(H_LEFT_RT,T_RIGTH_RT), 'i'):-
+	length([H_LEFT |T_LEFT], LENGTH),
+	((LENGTH > 0) ->
+	reverse([H_LEFT |T_LEFT], REVERSE_RT),
+	try1(REVERSE_RT, CHAIN1, TRY_RT),
+	reverse(CHAIN1, [H_AUX| T_AUX]),
+	append([H_AUX], T_RIGTH, APPEND_RT),
+	append(T_AUX, APPEND_RT, T_RIGTH_RT),
+	reverse(TRY_RT, AUX),
+	append(AUX, [], H_LEFT_RT)).
+
+movimiento(estado(T_LEFT, T_RIGTH), estado(H_LEFT_RT,T_RIGTH_RT), 'ddd'):-
+	length(T_RIGTH, LENGTH),
+	((LENGTH > 2) ->
+	try3(T_RIGTH, [A, B, C], AUX),
+	append(T_LEFT, [C], AUX2),
+	append(AUX2, [A,B], H_LEFT_RT),
+        append(AUX, [], T_RIGTH_RT)).
+
+movimiento(estado(T_LEFT, T_RIGTH), estado(H_LEFT_RT,T_RIGTH_RT), 'dd'):-
+	length(T_RIGTH, LENGTH),
+	((LENGTH > 1) ->
+	try2(T_RIGTH, [A, B], AUX),
+	append(T_LEFT, [B], AUX2),
+	append(AUX2, [A], H_LEFT_RT),
+        append(AUX, [], T_RIGTH_RT)).
+
+movimiento(estado(T_LEFT, T_RIGTH), estado(H_LEFT_RT,T_RIGTH_RT), 'd'):-
+	length(T_RIGTH, LENGTH),
+	((LENGTH > 0) ->
+	try1(T_RIGTH, [A], AUX),
+	append(T_LEFT, [A], AUX2),
+	append(AUX2, [], H_LEFT_RT),
+        append(AUX, [], T_RIGTH_RT)).
