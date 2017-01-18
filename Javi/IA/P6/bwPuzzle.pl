3d97bb20e3a60ae0926fd942c8d8f0da1b35ca1d
Javier C. Tejada <josejaco@ucm.es>
2017-01-18 13:40:23 +0100

    Update bwPuzzle.pl

--- Javi/IA/P6/bwPuzzle.pl
+++ Javi/IA/P6/bwPuzzle.pl
@@ -11,3 +11,18 @@ movimiento(estado([XL | XLS], [XR|XRS]), estado([YL | YLS],[YR|YRS])):-
 	append(P1OKS, AUX, [YR|YRS]),
 	reverse(P2, AUX2),
 	append(AUX2, [], [YL|YLS])).
+
+
+/*
+ * OPERADORES: movimiento(Estado, EstadoSiguiente)
+ */
+movimiento(estado([H_LEFT | T_LEFT], [H_RIGTH | T_RIGTH]), estado([H_LEFT_RT | T_LEFT_RT],[H_RIGTH_RT|T_RIGTH_RT])):-
+	length([H_LEFT |T_LEFT], LENGTH),
+	((LENGTH > 2) ->
+	reverse([H_LEFT |T_LEFT], REVERSE_RT),
+	try(REVERSE_RT, CHAIN1, TRY_RT),
+	reverse(CHAIN1, [H_AUX| T_AUX]),
+	append([H_AUX], [H_RIGTH | T_RIGTH], APPEND_RT),
+	append(T_AUX, APPEND_RT, [H_RIGTH_RT | T_RIGTH_RT]),
+	reverse(TRY_RT, AUX),
+	append(AUX, [], [H_LEFT_RT|T_LEFT_RT])).
