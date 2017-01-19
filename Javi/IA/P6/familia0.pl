23e662a595657915bacda2c0e46c288c54eabdb5
Javier C. Tejada <josejaco@ucm.es>
2017-01-19 15:24:00 +0100

    Update familia0.pl

--- Javi/IA/P6/familia0.pl
+++ Javi/IA/P6/familia0.pl
@@ -1,10 +1,18 @@
+/*
+ * Hechos iniciales
+ */
+
 dd(juan, maria, rosa, m).
 dd(juan, maria, luis, h).
 dd(jose, laura, pilar, m).
 dd(luis, pilar, miguel, h).
 dd(miguel,isabel,jaime,h).
 dd(pedro, rosa, pablo, h).
-dd(pedro, rosa, begoña, m).
+dd(pedro, rosa, begonia, m).
+
+/*
+ * Predcados definidos en base a hechos iniciales
+ */
 
 padre(X, Y):- dd(X, _, Y, _).
 madre(X, Y):- dd(_, X, Y, _).
@@ -13,51 +21,73 @@ hijo(X, Y):- dd(_, Y, X, h).
 hija(X, Y):- dd(Y, _, X, m).
 hija(X, Y):- dd(_, Y, X, m).
 
+/*
+ * Predicados definidos en base a los predicados anteriores
+ */
+
 abuelo(X, Y):- padre(X, Z),
-				padre(Z, Y).
+	       padre(Z, Y).
+
 abuelo(X, Y):- padre(X, Z),
-				madre(Z, Y).
+	       madre(Z, Y).
+
 abuela(X, Y):- madre(X, Z),
-				padre(Z, Y).
+	       padre(Z, Y).
+
 abuela(X, Y):- madre(X, Z),
-				madre(Z, Y).
+	       madre(Z, Y).
+
 hermano(X, Y):- hijo(X, Z),
-				hijo(Y, Z).
+		hijo(Y, Z).
+
 hermano(X, Y):- hijo(X, Z),
-				hija(Y, Z).
+		hija(Y, Z).
+
 hermana(X, Y):- hija(X, Z),
-				hijo(Y, Z).
+		hijo(Y, Z).
+
 hermana(X, Y):- hija(X, Z),
-				hija(Y, Z).		
+		hija(Y, Z).
+
 primo(X, Y):- hijo(X, Z),
-				hijo(Y, K),
-				hermano(Z, K).
+	      hijo(Y, K),
+	      hermano(Z, K).
+
 primo(X, Y):- hijo(X, Z),
-				hija(Y, K),
-				hermano(Z, K).
+	      hija(Y, K),
+	      hermano(Z, K).
+
 primo(X, Y):- hijo(X, Z),
-				hijo(Y, K),
-				hermana(Z, K).
+	      hijo(Y, K),
+	      hermana(Z, K).
+
 primo(X, Y):- hijo(X, Z),
-				hija(Y, K),
-				hermana(Z, K).
+	      hija(Y, K),
+	      hermana(Z, K).
 
 prima(X, Y):- hija(X, Z),
-				hijo(Y, K),
-				hermano(Z, K).
+	      hijo(Y, K),
+	      hermano(Z, K).
+
 prima(X, Y):- hija(X, Z),
-				hija(Y, K),
-				hermano(Z, K).
+	      hija(Y, K),
+	      hermano(Z, K).
+
 prima(X, Y):- hija(X, Z),
-				hijo(Y, K),
-				hermana(Z, K).
+	      hijo(Y, K),
+	      hermana(Z, K).
+
 prima(X, Y):- hija(X, Z),
-				hija(Y, K),
-				hermana(Z, K).
+	      hija(Y, K),
+	      hermana(Z, K).
+
 ascendiente(X, Y):- padre(X, Y).
 ascendiente(X, Y):- padre(X, Z),
-					ascendiente(Z, Y).
-					
+		    ascendiente(Z, Y).
+
 ascendiente(X, Y):- madre(X, Y).
 ascendiente(X, Y):- madre(X, Z),
-					ascendiente(Z, Y).
+		    ascendiente(Z, Y).
+
+
+
